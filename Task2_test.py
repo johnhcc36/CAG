06a3bb9b57fe1844b6b74a9ef5e85fc1360119bf
johnhcc36 <129492303+johnhcc36@users.noreply.github.com>
2023-03-31 18:01:13 +0800

    Delete Task2_test.py

--- Task2_test.py
+++
@@ -1,9 +0,0 @@
-# Insert some items
-insert_or_update_item('Notebook', 'Stationary', 5.5)
-insert_or_update_item('Key Chain', 'Gift', 3)
-insert_or_update_item('Baggage Cover', 'Gift', 15)
-
-# Get items within a date range
-date_range = {'dt_from': '2022-01-01 10:00:00', 'dt_to': '2022-01-25 10:00:00'}
-result = get_items_within_date_range(date_range['dt_from'], date_range['dt_to'])
-print(json.dumps(result, indent=4))
